76e5ef0d7117823012468001975bf9efe954f627
WolfBolin <wolfbolin@foxmail.com>
2019-07-26 00:04:02 +0800

    Update
    Switch to bootstrap

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,15 +1,26 @@
+console.log(
+    " __          __   _  __ ____        _ _  \n" +
+    " \\ \\        / /  | |/ _|  _ \\      | (_)  \n" +
+    "  \\ \\  /\\  / /__ | | |_| |_) | ___ | |_ _ __  \n" +
+    "   \\ \\/  \\/ / _ \\| |  _|  _ < / _ \\| | | '_ \\  \n" +
+    "    \\  /\\  / (_) | | | | |_) | (_) | | | | | |  \n" +
+    "     \\/  \\/ \\___/|_|_| |____/ \\___/|_|_|_| |_|  \n" +
+    "=================================================");
+console.log("Welcome to WolfBolin.com ~ \nContact me at: mailto@wolfbolin.com");
 $(function() {
     function cover_screen() {
-        $('.cover-title').css('padding', '0');
-        clientHeight = document.documentElement.clientHeight;
-        console.log(clientHeight)
-        titleHeight = $('.cover-title').height();
-        titlePadding = (clientHeight - titleHeight) / 2;
-        $('.cover-title').css('padding', titlePadding + 'px 0');
-        $('#explore').attr("data-am-smooth-scroll", "{position: " + (clientHeight - 60) + "}");
+        let exploreBtn = $("#explore");
+        let coverTitle = $("#cover-title");
+        let clientHeight = document.documentElement.clientHeight;
+        let titleHeight = coverTitle.height();
+        let titlePadding = (clientHeight - titleHeight) / 2;
+        coverTitle.css("padding", titlePadding + "px 0");
+        exploreBtn.click(function () {
+            $(window).scrollTo(clientHeight - 60, 400);
+        })
     }
     cover_screen();
-    $(window).resize(function() {
+    $(window).resize(function () {
         cover_screen();
     });
 });
