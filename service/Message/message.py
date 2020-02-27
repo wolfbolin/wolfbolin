d588d03a7e4ec85619d6526189e091adbe1e6b85
wolfbolin <wolfbolin@foxmail.com>
2020-02-27 17:35:59 +0800

    Auto send sms

--- service/Message/message.py
+++ service/Message/message.py
@@ -5,14 +5,15 @@ import pymemobird
 from flask import request
 from flask import current_app
 
-g_printer_message_key = {'app', 'user', 'text'}
+g_push_message_key = {'app', 'user', 'text'}
+g_send_message_key = {'phone', 'template', 'params'}
 
 
 @Message.message_blue.route("/printer/text", methods=["POST"])
 def printer_text_message():
     message_info = request.get_json()
 
-    if message_info is None or set(message_info.keys()) != g_printer_message_key:
+    if message_info is None or set(message_info.keys()) != g_push_message_key:
         return Util.common_rsp("Reject request", status='Forbidden')
 
     app = message_info['app']
@@ -42,16 +43,45 @@ def printer_text_message():
 
 
 @Message.message_blue.route("/sms/text", methods=["POST"])
-def sms_text_message():
+def sms_message_push():
     message_info = request.get_json()
 
-    if message_info is None or set(message_info.keys()) != g_printer_message_key:
+    if message_info is None or set(message_info.keys()) != g_push_message_key:
         return Util.common_rsp("Reject request", status='Forbidden')
 
     sms_arg = {
         "phone_numbers": [current_app.config['PHONE']['wolfbolin']],
         "template": current_app.config['SMS']['message_push'],
         "params": [message_info['app'], message_info['user'], message_info['text'][:12]]
+    }
+    sms_res, sms_msg = Util.send_sms_message(**sms_arg)
+
+    if sms_res:
+        return Util.common_rsp(sms_msg['detail'][0])
+    else:
+        return Util.common_rsp(sms_msg, status="Bad Gateway")
+
+
+@Message.message_blue.route("/sms/send/<phone>", methods=["POST"])
+def sms_message_send(phone):
+    message_info = request.get_json()
+
+    if message_info is None or set(message_info.keys()) != g_send_message_key:
+        return Util.common_rsp("Reject request", status='Forbidden')
+
+    if phone is None or phone != message_info["phone"] or len(phone) != 11:
+        return Util.common_rsp("Reject request", status='Forbidden')
+
+    if isinstance(message_info["params"], list):
+        for i, item in enumerate(message_info["params"]):
+            message_info["params"][i] = str(item)[:12]
+    else:
+        return Util.common_rsp("Reject request", status='Forbidden')
+
+    sms_arg = {
+        "phone_numbers": [str(message_info["phone"])],
+        "template": str(message_info["template"]),
+        "params": message_info["params"]
     }
     sms_res, sms_msg = Util.send_sms_message(**sms_arg)
 
