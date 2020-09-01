a4870289808be4f2d9c576fcd9fdf9522b717de6
WolfBolin <zhangbolin@xiaomi.com>
2020-09-01 10:14:47 +0800

    Docker bash

--- service/make-docker.sh
+++ service/make-docker.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+docker_name="core-wolfbolin"
 docker image prune -f
-docker stop core-wolfbolin
-docker rm core-wolfbolin
+docker stop ${docker_name}
+docker rm ${docker_name}
 echo -e "\033[5;36mOrz 旧容器(镜像)已清理\033[0m"
 
 time_now=$(date "+%m%d%H")
-docker build -f Dockerfile --tag core-wolfbolin:"${time_now}" .
+docker build -f Dockerfile --tag ${docker_name}:"${time_now}" .
 echo -e "\033[5;36mOrz 镜像重建完成\033[0m"
 
 docker run -itd \
-	-p 12864:80 \
 	--restart always \
-	--name core-wolfbolin\
-	-v "$(pwd)":/var/app \
-	core-wolfbolin:"${time_now}"
+	--name ${docker_name} \
+	-v $(pwd):/var/app \
+	-p 12880:12880 \
+	${docker_name}:"${time_now}"
 echo -e "\033[5;36mOrz 镜像启动完成\033[0m"
 docker ps -a
-docker logs core-wolfbolin -f
+docker logs ${docker_name} -f
